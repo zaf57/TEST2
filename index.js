60662e3f3620a86c93d83d00b2e3a5ff4d6f19e9
zaf57 <41347008+zaf57@users.noreply.github.com>
2023-07-03 00:20:52 +0200

    index.js

---
+++ index.js
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ma première page HTML</title>
+    <style>
+        /* Votre CSS ici */
+    </style>
+    <script>
+        var correspondances = {}; // Variable pour stocker les correspondances de codes de pneus et de hauteurs
+
+        function afficherHauteur() {
+            var codePneu = document.getElementById("code-pneu").value;
+            var hauteurPneu = document.getElementById("hauteur-pneu");
+
+            if (correspondances.hasOwnProperty(codePneu)) {
+                hauteurPneu.value = correspondances[codePneu];
+            } else {
+                hauteurPneu.value = "";
+            }
+        }
+
+        function chargerCorrespondances(event) {
+            var fichier = event.target.files[0];
+            var lecteur = new FileReader();
+
+            lecteur.onload = function(e) {
+                var contenu = new Uint8Array(e.target.result);
+                var classeur = XLSX.read(contenu, { type: 'array' });
+
+                var feuille = classeur.Sheets[classeur.SheetNames[0]];
+                var donnees = XLSX.utils.sheet_to_json(feuille);
+
+                correspondances = {}; // Réinitialiser les correspondances
+
+                donnees.forEach(function(d) {
+                    correspondances[d.code_pneu] = d.hauteur;
+                });
+
+                console.log(correspondances);
+            };
+
+            lecteur.readAsArrayBuffer(fichier);
+        }
+    </script>
+</head>
+<body>
+    <h1>Titre de ma page</h1>
+
+    <input type="file" id="fichier-correspondances" accept=".xlsx" onchange="chargerCorrespondances(event)">
+
+    <br><br>
+
+    <label for="code-pneu">Code pneu:</label>
+    <input type="text" id="code-pneu" placeholder="Entrez le code du pneu" onkeyup="afficherHauteur()">
+
+    <br><br>
+
+    <label for="hauteur-pneu">Hauteur du pneu:</label>
+    <input type="text" id="hauteur-pneu" readonly>
+</body>
+</html>
